b85bfdb54246a015d61e508d9544480778432cba
Ambush Neupane <ambush@localhost.localdomain>
2022-01-31 23:05:43 +0545

    Removed unwanted part

--- dayFourteen/pointers11.c
+++ dayFourteen/pointers11.c
@@ -1,13 +1,12 @@
 // This doesn't work as intended 
 #include <stdio.h>
  
-int sumOfElem(int a[]){
+int sumOfElem(int *a){  // array decay. 
     int i, sum =0;
     int size = sizeof(a)/sizeof(a[0]);
     printf("SOE~ Size of a= %d , Size of a[0] = %d\n",sizeof(a),sizeof(a[0]));
 
     for (i=0;i<size;i++){
-        printf("%d\n",a[i]);
         sum +=a[i];
     }
     return sum;
